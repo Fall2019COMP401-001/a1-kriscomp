a2808afdca8de1dc55519a36c91115c05efd5376
kriscomp <kriscomp@host.docker.internal>
2019-09-02 09:32:56 -0400

    variable

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,27 +8,32 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 		
-		int personNumber, itemNumber;
-		char firstInitial;
-		String[] name = new String[9999];
-		String[] costListStr = new String[9999];
-		double[] cost = new double[9999];
+		int personNumber;
+		int itemNumber;
+		char initial;
+		
+		String[] name = new String[999];
+		String[] costListStr = new String[999];
+		double[] cost = new double[999];
 		String read;
 		
 		personNumber = Integer.valueOf(scan.next());
 		for (int k = 1; k <= personNumber; k ++) {
-			firstInitial = scan.next().charAt(0);
-			name[k] = firstInitial + ". " + scan.next();
+			initial = scan.next().charAt(0);
+			name[k] = initial + ". " + scan.next();
 			itemNumber = Integer.valueOf(scan.next());
-			for (int i = 0; i < itemNumber; i ++) {
+			
+			for (int i = 1; i <= itemNumber; i ++) {
 				read = scan.next();
 				scan.next();
 				cost[k] += Double.valueOf(scan.next()) * Integer.valueOf(read);
 			}
+			
 			costListStr[k] = String.format("%.2f", cost[k]);
 		}
 		scan.close();
-		for (int j = 0; j < personNumber; j ++) {
+		
+		for (int j = 1; j <= personNumber; j ++) {
 			System.out.println(name[j] + ": "+ costListStr[j]);
 		}
 	}
