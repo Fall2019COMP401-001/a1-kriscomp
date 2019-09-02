4b723e6e3830d36a9340f36f3aeeb4aa2f30f130
kriscomp <kriscomp@host.docker.internal>
2019-09-02 00:37:17 -0400

    try 1

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -6,9 +6,29 @@ public class A1Novice {
 
 	public static void main(String[] args) {
 		
+		int personNumber, itemNumber;
+		char firstInitial;
+		String[] name = new String[9999];
+		String[] costListStr = new String[9999];
+		double[] cost = new double[9999];
+		String read;
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		personNumber = Integer.valueOf(scan.next());
+		for (int k = 1; k <= personNumber; k ++) {
+			firstInitial = scan.next().charAt(0);
+			name[k] = firstInitial + ". " + scan.next();
+			itemNumber = Integer.valueOf(scan.next());
+			for (int i = 0; i < itemNumber; i ++) {
+				read = scan.next();
+				scan.next();
+				cost[k] += Double.valueOf(scan.next()) * Integer.valueOf(readIn);
+			}
+			costListStr[i] = String.format("%.2f", cost[i]);
+		}
+		scan.close();
+		for (int j = 0; j < personNumber; j ++) {
+			System.out.println(name[j] + ": "+ costListStr[i]);
+		}
 	}
 }
